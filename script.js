b1bb437b00dedbca81ed66ceac5d2ca2ad9f50b4
Dilshan <dopatha@nc.rr.com>
2020-04-10 19:23:12 -0400

    finishing js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var hour= moment().hours()
+// console.log(hour)
 function getTime() {
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 }
@@ -6,15 +8,31 @@ $(document).ready(function() {
     setInterval(getTime, 1000)
 });
 
-var hour= moment().hours()
-var row= hour
-$("description").each(function() {
-    if (row === hour) {
+$(".description").each(function() {
+    var rowHour= $(this).parent().attr("id")
+    var rowNumber= parseInt(rowHour);
+    // console.log(rowNumber)
+    if (rowNumber === hour) {
         $(this).addClass("present");
     }
-        else if (row < hour) {
+        else if (rowNumber < hour) {
         $(this).addClass("past");
     
     }
+        else {
+            $(this).addClass("future");
+        };
 
+});
+
+$(".saveBtn").click(function(){
+    var message= $(this).siblings(".description").val();
+    var locationId= $(this).parent().attr("id");
+    localStorage.setItem(locationId, message);
+    // console.log(locationId, message)
+});
+
+$(".description").each(function() {
+    var id= $(this).parent().attr("id");
+    $(this).val(localStorage.getItem(id));
 });
